812ded4d96e7a572d2065e8d09b5833be5ced3b8
Udit Narayan Singh <uditn007@gmail.com>
2025-10-03 12:23:14 +0530

    setup the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { ScrollTrigger } from 'gsap/all'
+import gsap from 'gsap'
+gsap.registerPlugin(ScrollTrigger)
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-pink-600'>hedfh;ljksa</h1>
+    </>
+  )
+}
+
+export default App
